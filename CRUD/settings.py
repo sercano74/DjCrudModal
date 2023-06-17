df890f6d8866dce064591142499c84194cd16e49
sercanno <Sergio.cannobbio@inacapmail.cl>
2023-06-17 15:57:44 -0400

    first commit

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -35,20 +35,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-imbqmbsbmz-__klci&i=o5b#3u!viwr5rmpov-e+$&^9f6#jg8'
-# SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-imbqmbsbmz-__klci&i=o5b#3u!viwr5rmpov-e+$&^9f6#jg8')
+# SECRET_KEY = 'django-insecure-imbqmbsbmz-__klci&i=o5b#3u!viwr5rmpov-e+$&^9f6#jg8'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-imbqmbsbmz-__klci&i=o5b#3u!viwr5rmpov-e+$&^9f6#jg8')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = []
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
